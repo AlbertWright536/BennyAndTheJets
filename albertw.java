2dd82c892b2a3d9a9512598d0167039c482b187d
Albert M Wright <albertmwright@gmail.com>
2025-08-28 15:09:34 -0400

    Update for Tavish

--- albertw.java
+++ albertw.java
@@ -1,5 +1,12 @@
-public class albertw {
-    public static void main(String[] args){
-        System.out.println("albert m wright");
+/**
+ * This program is designed to ensure that you can write,
+ * compile, and submit a program for CS1131.
+ * 
+ * @author Albert M Wright
+ * 
+ */
+public class AlbertW {
+public static void main(String[] args) {
+    	System.out.println("Albert M Wright");
     }
 }
