070379110c9b9a5afc793d5785d7f15a7f681868
Albert M Wright <albertmwright@gmail.com>
2025-09-18 15:07:02 -0400

    update

---
+++ Program4.java
@@ -0,0 +1,11 @@
+import java.util.scanner;
+/**
+ * * Program Name: Program 4
+ * * Program Description: Translate English to Pirate
+ * * Course: CS 1131
+ * * Lab Section 8:00 am -10:00 am
+ * * Lab Team Name: Bennie And The Jets
+ * * @author Marcus B, Michelle B, Freya D, Albert W
+ */
+
+public String translateEnglish2Pirate(String englishSentence);
