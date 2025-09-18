91d8c646d2768d7dda8a41adc32abef5d46406f8
Albert M Wright <albertmwright@gmail.com>
2025-09-18 15:05:31 -0400

    got the basic comments in

---
+++ Week4Program.java
@@ -0,0 +1,9 @@
+import java.util.scanner;
+/**
+ * * Program Name: Program 4
+ * * Program Description: Translate English to Pirate
+ * * Course: CS 1131
+ * * Lab Section 8:00 am -10:00 am
+ * * Lab Team Name: Bennie And The Jets
+ * * @author Marcus B, Michelle B, Freya D, Albert W
+ */	
