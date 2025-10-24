3fd2262577e7e1eeeb92596d77239c30eec5bd1a
Albert M Wright <albertmwright@gmail.com>
2025-10-24 15:44:47 -0400

    Help Button functionality

--- BrowserProgram.java
+++ BrowserProgram.java
@@ -139,6 +139,11 @@ public class BrowserProgram extends Application {
 		});
 
 		Button help = new Button("?");
+		help.setOnAction(e -> {
+			webEngine.loadContent("<html><body<h1>WebBrowser</H1><br/></body></html>You are using the web browser application developed by BennyAndTheJets, a team of students at Michigan Technological University. This was developed as part of the class CS1131 Accelerated Intro to Programming, Lab session L03. To use this program type a web address in the search bar at the top. If the address that you typed was invalid you will be routed to this screen. If you were routed here then it is likely that you made an error when typing the URL. Make sure that you remember to include https:// at the start of your URL, and .com at the end. If you are unable to remember the URL consider navigating to https://google.com to find the sight that way");
+		
+			}
+		);
 		toolbar.setHgrow(addressBar, Priority.ALWAYS);
 		toolbar.getChildren().addAll(backArrow, forwardArrow, addressBar, help);
 		return toolbar;
