bed4e178c84457ff3cf0a55218a59abe93dd96aa
Freya Durst <freyadurst@worldofmail.com>
2025-10-30 15:58:16 -0400

    Initial commit

---
+++ FractalApp.java
@@ -0,0 +1,48 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.stage.Stage;
+import java.util.List;
+
+public class FractalApp extends Application {
+    @Override
+
+    public void start(Stage stage) {
+        List<String> args = getParameters().getRaw();
+
+        Pane pane = new Pane();
+        pane.setPrefSize(800, 600);
+        Scene scene = new Scene(pane, 800, 600, Color.WHITE);
+        stage.setScene(scene);
+        stage.setTitle("CS1131 Fractal");
+        stage.show();
+
+        drawFractal(pane, args);
+    }
+
+    private void drawFractal(Pane pane, List<String> args) {
+        sierpinskiIterate(pane, 0, 0, 400, 5);
+    }
+
+    private void sierpinskiIterate(Pane pane, double x, double y, double side, int depth) {
+        double half = side / 2;
+        double h = Math.sqrt(3) * half;
+        if (depth == 0) {
+            Polygon triangle = new Polygon();
+            triangle.getPoints().addAll(new Double[]{
+                x, y + h,
+                x + side, y + h,
+                x + half, y
+            });
+            pane.getChildren().add(triangle);
+            return;
+        }
+
+        sierpinskiIterate(pane, x + half/2, y, half, depth - 1);
+        sierpinskiIterate(pane, x, y + h/2, half, depth - 1);
+        sierpinskiIterate(pane, x + half, y + h/2, half, depth - 1);
+    }
+}
